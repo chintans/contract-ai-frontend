0f199c080e4f72d4f8dffc53b76990667bd086d6
Chintan Shah <chintanshah@promactinfo.com>
2025-05-06 14:35:55 +0530

    Update Cypress TypeScript configuration and enhance RuleDialogComponent. Removed unnecessary Chai type from Cypress config and added FormsModule to RuleDialogComponent imports for improved form handling.

--- src/app/features/templates/rule-dialog.component.ts
+++ src/app/features/templates/rule-dialog.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ClauseRule } from './template-version.model';
 
 @Component({
   selector: 'app-rule-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './rule-dialog.component.html',
   styleUrls: ['./rule-dialog.component.scss']
 })
 export class RuleDialogComponent {
-  @Input() rule: ClauseRule;
+  @Input() rule!: ClauseRule;
   @Output() save = new EventEmitter<ClauseRule>();
   @Output() cancel = new EventEmitter<void>();
 } 
