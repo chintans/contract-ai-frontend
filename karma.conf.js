80456f0800087dfcba9bc3e4d0d39b09977ff4d7
Chintan Shah <chintanshah@promactinfo.com>
2025-05-22 10:19:40 +0530

    chore: update Karma configuration and dependencies
    - Changed browser setting from Chrome to ChromeHeadless in karma.conf.js for headless testing.
    - Updated package manager version in package.json to pnpm@10.11.0.
    - Added "ngrx" to the cSpell dictionary in .vscode/settings.json.

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
       }
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     restartOnFileChange: true
   });
 };
