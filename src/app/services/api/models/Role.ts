6572e1eade790d5e31d7c6c812dbdb53d0c28190
Chintan Shah <chintanshah@promactinfo.com>
2025-05-29 17:56:24 +0530

    feat(api): update API generation and schema definitions
    - Added a new script to generate an OpenAPI client for Angular.
    - Updated references in the Swagger JSON from `StandardClause` to `StandardClauseDto` for consistency.
    - Enhanced the Swagger documentation with additional descriptions for contract ID parameters.
    - Removed deprecated API-related files and services to streamline the codebase.

--- src/app/services/api/models/Role.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type Role = {
-    id: number;
-    name: string;
-};
-
